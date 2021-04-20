55f61c4a5108c239c6cd94c6c5247dece99d4986
luffyguy <harshtanwar25@gmail.com>
2021-04-20 23:01:21 +0530

    2nd done and fixed

--- Bp_sqrt.cpp
+++ Bp_sqrt.cpp
@@ -1,12 +1,24 @@
-#include<iostream>
-#include<cstdio>
-#include<cmath>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin >> n;
+
+    double a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << fixed << setprecision(2) << sqrt(a[i]) << endl;
+    }
     return 0;
 }
-
