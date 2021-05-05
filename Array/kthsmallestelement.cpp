445bd0a565168d9be7c8b92baafe075ac1c8052a
Harsh Tanwar <58739426+luffyguy@users.noreply.github.com>
2021-05-05 10:48:29 +0530

    kthj smallest element

--- Array/kthsmallestelement.cpp
+++ Array/kthsmallestelement.cpp
@@ -1,13 +1,27 @@
-#include<iostream>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
-int main(){
+int kthSmallest(int a[], int n, int k)
+{
+    sort(a, a + n);
+
+    return a[k - 1];
+}
+
+int main()
+{
     int n;
     cin>>n;
 
     int a[n];
-    for(int i=0;i<n;i++){
-        cin>>a[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
     }
-    
+    int k;
+    cin>>k;
+
+    cout << kthSmallest(a, n, k);
+    return 0;
 }
